411afbf91f677c01618061cbac2bf9b5c99bfe18
canary <222850000+R2xpdHRlcmluZyBDYW5hcnk@users.noreply.github.com>
2026-09-18 13:18:57 +0800

    chore: bump version to v0.1

--- cmd/ipv4-relay/main.go
+++ cmd/ipv4-relay/main.go
@@ -23,7 +23,7 @@ func ipv4Enabled() bool {
 	return true
 }
 
-const version = "v0.1-beta4"
+const version = "v0.1"
 
 func printUsage(app string, exitCode int) {
 	fmt.Fprintf(os.Stderr, "== %s Usage ==\n"+
